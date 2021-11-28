c8e514a4e96a1d4160a27f1b2d358d40d1789f2c
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-28 17:25:03 +0500

    Добавьте файлы проекта.

---
+++ Hospital queue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hospital_queue
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int timeOfReceipt = 10; //minutes
+            int minutesPerHour = 60;
+
+            Console.Write("Введите количество людей стоящих в очереди: ");
+            int numberOfPeople = Convert.ToInt32(Console.ReadLine());
+
+            int totalWaitingTime = numberOfPeople * timeOfReceipt;
+            Console.WriteLine($"Время ожидания составит: " +
+                $"{totalWaitingTime / minutesPerHour} часов" +
+                $" {totalWaitingTime % minutesPerHour} минут");
+        }
+    }
+}
