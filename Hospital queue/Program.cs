08ed21f0f34cc51fa9bc848b2f9d454bb106372d
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-28 23:26:03 +0500

    Fixed console output

--- Hospital queue/Program.cs
+++ Hospital queue/Program.cs
@@ -11,12 +11,14 @@ namespace Hospital_queue
 
             Console.Write("Введите количество людей стоящих в очереди: ");
             int numberOfPeople = Convert.ToInt32(Console.ReadLine());
+
             int totalWaitingTime = numberOfPeople * timeOfReceipt;
             int waitingHours = totalWaitingTime / minutesPerHour;
             int waitingMinutes = totalWaitingTime % minutesPerHour;
+
             Console.WriteLine($"Время ожидания составит: " +
                 $"{waitingHours} часов" +
-                $"{waitingMinutes} минут");
+                $" {waitingMinutes} минут");
         }
     }
 }
